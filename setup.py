125098b8b5feed2b06985aa0eebe45bfb64ff5d0
Simon Alibert <alibert.sim@gmail.com>
2024-04-02 12:13:44 +0200

    Update setup

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-
-setup(
-    name="gym_pusht",
-    version="0.0.1",
-    install_requires=["gymnasium", "opencv-python", "pygame", "pymunk", "shapely", "scikit-image"],
-)
