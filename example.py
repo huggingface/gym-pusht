0d281d4784794d5e9fe79e9478f34fd9bc27f6be
Simon Alibert <alibert.sim@gmail.com>
2024-04-08 12:13:46 +0200

    Add example & more tests

---
+++ example.py
@@ -0,0 +1,15 @@
+import gymnasium as gym
+import gym_pusht
+
+env = gym.make("gym_pusht/PushT-v0", render_mode="human")
+observation, info = env.reset()
+
+for _ in range(1000):
+    action = env.action_space.sample()
+    observation, reward, terminated, truncated, info = env.step(action)
+    image = env.render()
+
+    if terminated or truncated:
+        observation, info = env.reset()
+
+env.close()
--- gym_pusht/__init__.py
+++ gym_pusht/__init__.py
@@ -6,11 +6,3 @@ register(
     max_episode_steps=300,
     kwargs={"obs_type": "state"},
 )
-
-
-register(
-    id="gym_pusht/PushTPixels-v0",
-    entry_point="gym_pusht.envs:PushTEnv",
-    max_episode_steps=300,
-    kwargs={"obs_type": "pixels"},
-)
